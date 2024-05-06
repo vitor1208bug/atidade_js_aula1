ca46c9db9c79844f2c2f11555cc9d2647bbc5009
VitorSousa.2 <vitorgg1208bug@gmail.com>
2024-05-06 18:58:31 -0300

    Add files via upload

---
+++ atidade Js/dom.js
@@ -0,0 +1,25 @@
+let object = {
+    nome: 'virto',
+    idade: 18,
+    humano: true
+
+}
+
+console.log(`nome ${object.nome} idade ${object.idade}`)
+
+
+let comidas = ['pizza', "rosquinha", "sorvete",
+            {
+                nome: "vitor",
+                idade: 18
+            }
+]
+
+console.log(`seu nome é ${comidas[3].nome}`)
+
+
+let dicionario = new Map();
+
+dicionario.set("cachorro", "boris")
+
+
